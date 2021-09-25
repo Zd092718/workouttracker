0a72179a5e9947dda4c8afe355b721e779620804
Zachary Dowd <zacharyd2796@zohomail.com>
2021-09-25 11:36:47 -0600

    model created

---
+++ models/workoutModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  type: {
+    type: String,
+    trim: true,
+    required: "Exercise Type is Required",
+  },
+
+  name: {
+    type: String,
+    trim: true,
+    required: "Exercise Name is Required",
+  },
+
+  duration: {
+    type: Number,
+    required: "Exercise Duration is Required",
+  },
+
+  weight: {
+    type: Number,
+    required: "Exercise Weight is Required",
+  },
+
+  reps: {
+    type: Number,
+    required: "Exercise Reps are Required",
+  },
+
+  sets: {
+    type: Number,
+    required: "Exercise Sets are Required",
+  },
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
