487e1ed29cd7a6fddbaf172901d62caa42d65bd1
Zachary Dowd <zacharyd2796@zohomail.com>
2021-09-26 15:00:31 -0600

    get ranges route created

--- routes/api/workoutRoutes.js
+++ routes/api/workoutRoutes.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const db = require("../../models/workout");
 
-router.get("/", (req, res) => {
-  db.Workout.find({}, (error, data) => {
+//gets all workouts
+router.get("/", async (req, res) => {
+  await db.Workout.find({}, (error, data) => {
     if (error) {
       res.send(error);
     } else {
@@ -10,8 +11,10 @@ router.get("/", (req, res) => {
     }
   });
 });
-router.put("/:id", (req, res) => {
-  db.Workout.update({}, (error, data) => {
+
+//gets all workouts in range
+router.get("/range", async (req, res) => {
+  await db.Workout.find({}, (error, data) => {
     if (error) {
       res.send(error);
     } else {
@@ -19,14 +22,27 @@ router.put("/:id", (req, res) => {
     }
   });
 });
-router.post("/", (req, res) => {
-  db.Workout.find({}, (error, data) => {
+
+// add exercise
+router.put("/:id", async (req, res) => {
+  await db.Workout.updateOne({}, (error, data) => {
     if (error) {
       res.send(error);
     } else {
       res.json(data);
     }
   });
+});
+
+//post route for new workout
+router.post("/", async ({ body }, res) => {
+  await db.Workout.create(body)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 // console.log(Workout.find({}));
 module.exports = router;
