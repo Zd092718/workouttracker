56acc47d999f930a5a67abcf7b3cacad5b63f7ca
Zachary Dowd <zacharyd2796@zohomail.com>
2021-09-26 20:02:09 -0600

    routes changed

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,10 @@ const path = require("path");
 
 router.use("/api", apiRoutes);
 
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
 module.exports = router;
